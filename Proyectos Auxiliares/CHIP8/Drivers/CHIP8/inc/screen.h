906d59263db80aa004f7c24162156e57699c51c6
LopLuAl <lopez.luciano96a@gmail.com>
2021-09-22 22:57:56 -0300

    Agrego configuracion HWD CHIP 8

--- Proyectos Auxiliares/CHIP8/Drivers/CHIP8/inc/screen.h
+++ Proyectos Auxiliares/CHIP8/Drivers/CHIP8/inc/screen.h
@@ -4,10 +4,8 @@
 
 #include "main.h"
 
-#define WINDOW_HEIGHT 640
-#define WINDOW_WIDTH 1280
 
 void draw_graphics(uint32_t *buffer);
-void buffer_graphics(Chip8 *chip8, uint32_t *buffer);
+
 
 #endif // SCREEN_H
